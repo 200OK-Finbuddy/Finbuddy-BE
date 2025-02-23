306b362b15ba579c3a13a8cc761639079f67e75b
Hyejeong <thswjd0609@gmail.com>
2025-02-23 18:18:01 +0900

    feat: 추천 예/적금 상품 기능

--- src/main/java/com/http200ok/finbuddy/product/repository/DepositProductRepository.java
+++ src/main/java/com/http200ok/finbuddy/product/repository/DepositProductRepository.java
@@ -5,10 +5,20 @@ import com.http200ok.finbuddy.product.domain.DepositProduct;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface DepositProductRepository extends JpaRepository<DepositProduct, Long> {
     Optional<DepositProduct> findByNameAndBank(String name, Bank bank);
     Page<DepositProduct> findAllByOrderByDisclosureStartDateDesc(Pageable pageable);
+
+    @Query("""
+        SELECT DISTINCT dp FROM DepositProduct dp
+        JOIN dp.options o
+        GROUP BY dp
+        ORDER BY MAX(o.maximumInterestRate) DESC, dp.disclosureStartDate DESC
+        """)
+    List<DepositProduct> findTop3DepositProductsByMaxInterestRate(Pageable pageable);
 }
