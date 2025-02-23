a8074b3a0e009d72ced71b58dc53dee155e4df7e
Hyejeong <thswjd0609@gmail.com>
2025-02-23 16:24:21 +0900

    feat: CrossOrigin 어노테이션 추가

--- src/main/java/com/http200ok/finbuddy/account/controller/AccountController.java
+++ src/main/java/com/http200ok/finbuddy/account/controller/AccountController.java
@@ -5,11 +5,9 @@ import com.http200ok.finbuddy.account.dto.CheckingAccountSummaryResponseDto;
 import com.http200ok.finbuddy.account.service.AccountService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/accounts")
 @RequiredArgsConstructor
