0e32e0f55b5cd83a10e732dc3a94cd5b83f31c1a
seyeonsim <seyeonsimw@gmail.com>
2025-02-28 15:02:00 +0900

    feat: 회원가입 기능 구현

--- src/main/java/com/http200ok/finbuddy/config/SecurityConfig.java
+++ src/main/java/com/http200ok/finbuddy/config/SecurityConfig.java
@@ -3,6 +3,9 @@ package com.http200ok.finbuddy.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,9 +15,13 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
-                .csrf(csrf -> csrf.disable()); // CSRF 보호 비활성화 (개발 중만 사용)
+                .csrf(AbstractHttpConfigurer::disable); // CSRF 보호 비활성화 (개발 중만 사용)
 
         return http.build();
     }
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
