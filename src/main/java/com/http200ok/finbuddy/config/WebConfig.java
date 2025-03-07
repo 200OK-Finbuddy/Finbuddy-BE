f78f8843e863de15715fcbe11c4f500da4fbc8d0
Hyejeong <thswjd0609@gmail.com>
2025-03-07 09:05:44 +0900

    feat: application.properties 설정

--- src/main/java/com/http200ok/finbuddy/config/WebConfig.java
+++ src/main/java/com/http200ok/finbuddy/config/WebConfig.java
@@ -10,7 +10,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("https://finbuddy.shop")
+                //.allowedOrigins("http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
