c855371458c6cd8bb5c394dda78dd4963a027a87
Junyoung Choi <pantomkid@naver.com>
2025-03-11 08:55:12 +0900

    fix: AutoTransferRepository 자동이체 목록 조회 수정

--- src/main/java/com/http200ok/finbuddy/transfer/repository/AutoTransferRepository.java
+++ src/main/java/com/http200ok/finbuddy/transfer/repository/AutoTransferRepository.java
@@ -12,7 +12,8 @@ public interface AutoTransferRepository extends JpaRepository<AutoTransfer, Long
     List<AutoTransfer> findByStatus(AutoTransferStatus status);
 
     // 특정 회원의 자동이체 목록 조회 (account.member.id 기준)
-    List<AutoTransfer> findByAccount_Member_Id(Long memberId);
+    @Query("SELECT at FROM AutoTransfer at JOIN FETCH at.account WHERE at.account.member.id = :memberId")
+    List<AutoTransfer> findByAccount_Member_Id(@Param("memberId") Long memberId);
 
     // 자동이체 날짜와 활성/비활성 상태
     List<AutoTransfer> findByTransferDayAndStatus(Integer transferDay, AutoTransferStatus status);
